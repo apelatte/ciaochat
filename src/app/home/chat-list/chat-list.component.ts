c7779a8f1c14e119e69f0e009d8c2eac134c78b5
apelatte <palapinto10@gmail.com>
2025-03-20 12:39:35 +0100

    feat(menu): add friend modal

--- src/app/home/chat-list/chat-list.component.ts
+++ src/app/home/chat-list/chat-list.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
+import { MenuComponent } from './menu/menu.component';
+import { CommonModule } from '@angular/common';
+import { AddFriendComponent } from './add-friend/add-friend.component';
 
 @Component({
   selector: 'app-chat-list',
-  imports: [],
+  imports: [MenuComponent, CommonModule, AddFriendComponent],
   templateUrl: './chat-list.component.html',
   styleUrl: './chat-list.component.css'
 })
 export class ChatListComponent {
+
+  showMoreMenu: boolean = false;
+
+  constructor(private eRef: ElementRef){}
+
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: Event) {
+    // Si el clic ocurre fuera del botón o del menú, oculta el menú
+    if (this.showMoreMenu && !this.eRef.nativeElement.contains(event.target)) {
+      this.showMoreMenu = false;
+    }
+  }
+
+  toggleMenu() {
+    this.showMoreMenu = !this.showMoreMenu;
+  }
 
 }
